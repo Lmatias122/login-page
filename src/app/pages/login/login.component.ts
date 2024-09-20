b64c78ee649a69fd0c7223f88c12d92bce8940c2
Leonardo <leomatias.lm@gmail.com>
2024-09-20 15:59:33 -0300

    Finish Front Login Page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +25,8 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastService: ToastrService
   ){
     this.loginForm = new FormGroup({
       email: new FormControl('',[Validators.required,Validators.email]),
@@ -34,8 +36,8 @@ export class LoginComponent {
 
   submit(){
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-    next: () => console.log("sucesso"),
-    error: () => console.log("error")
+    next: () => this.toastService.success("Login Feito com Sucesso"),
+    error: () => this.toastService.error("Erro Inesperado! Tente novamente mais tarde")
     })
   }
 
